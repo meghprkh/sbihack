ec4ae7c0edffeffa5aee1a8cd24573d24364efb3
mprocks <saurabhravindranath@gmail.com>
2017-11-13 09:57:27 +0530

    Added rough code to validate identity

--- ml.py
+++ ml.py
@@ -1,8 +1,72 @@
+import time
+
+start = time.time()
+
+import argparse
+import cv2
+import itertools
+import os
+
+import numpy as np
+np.set_printoptions(precision=2)
+
+import openface
+
+fileDir = os.path.dirname(os.path.realpath(__file__))
+modelDir = os.path.join(fileDir, '..', 'models')
+dlibModelDir = os.path.join(modelDir, 'dlib')
+openfaceModelDir = os.path.join(modelDir, 'openface')
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('imgs', type=str, nargs='+', help="Input images.")
+parser.add_argument('--dlibFacePredictor', type=str, help="Path to dlib's face predictor.",
+                    default=os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat"))
+parser.add_argument('--networkModel', type=str, help="Path to Torch network model.",
+                    default=os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'))
+parser.add_argument('--imgDim', type=int,
+                    help="Default image dimension.", default=96)
+
+args = parser.parse_args()
+
+start = time.time()
+align = openface.AlignDlib(args.dlibFacePredictor)
+net = openface.TorchNeuralNet(args.networkModel, args.imgDim)
+
 def get_features(impath):
-    pass
+    bgrImg = cv2.imread(imgPath)
+    if bgrImg is None:
+        raise Exception("Unable to load image: {}".format(imgPath))
+    rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
+
+    bb = align.getLargestFaceBoundingBox(rgbImg)
+    if bb is None:
+        raise Exception("Unable to find a face: {}".format(imgPath))
+
+    alignedFace = align.align(args.imgDim, rgbImg, bb,
+                              landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
+    if alignedFace is None:
+        raise Exception("Unable to align image: {}".format(imgPath))
+
+    rep = net.forward(alignedFace)
+    return rep
 
 def do_faces_match(imfeatures1, imfeatures2):
-    pass
+    d = imfeatures1 - imfeatures2
+    d = np.dot(d, d)
+   	return d <= 1
 
 def is_alive(imfeaturesprev, imfeaturesnew):
     pass
+
+orig = get_features(retrieved_image)
+start = time.time()
+cnt_match, cnt_total = 0, 0
+while 1:
+	if(time.time() - start > 1):
+		start = time.time()
+		new = get_features('img104_656_276.jpg')
+		if(do_faces_match(orig, new) == 1):
+			cnt_match += 1
+		cnt_total += 1
+
